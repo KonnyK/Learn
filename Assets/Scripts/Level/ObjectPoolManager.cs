5b5fc200cffeab59e7b46b97fdbc0b03c907f9e5
Konny <konrad.kroeber@freenet.de>
2018-10-22 22:21:47 +0200

    evening

---
+++ Assets/Scripts/Level/ObjectPoolManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ObjectPoolManager : NetworkBehaviour {
+
+    [ClientRpc]
+	public void RpcClear()
+    {
+        foreach (Transform Child in transform) Destroy(Child.gameObject);
+    }
+}
