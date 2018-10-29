d25070c7b99d7648d150ccbd5243b399dfb220d9
Konny <konrad.kroeber@freenet.de>
2018-10-29 21:13:44 +0100

    MO

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Player : MonoBehaviour {
+public class Player : NetworkBehaviour {
 
     [SerializeField] private string P_Name;
     [SerializeField] private int P_Number;//PlayerNumber
@@ -15,11 +16,10 @@ public class Player : MonoBehaviour {
     [SerializeField] private bool Invincible; //used for Checkpoints
 
     [SerializeField] private Vector3 SpawnPos; //Spawnpoint
-    [SerializeField] private Quaternion SpawnRot; //Spawnrotation
     [SerializeField] private readonly int RespawnTime = 3; //Time it will take for the Player to respawn once respawning was started
     [SerializeField] private uint Deathcount = 0; //how many times the Player died
-    [SerializeField] private float TimeofDeath;
 
+    [SerializeField] private Game_Manager GameManager;
     [SerializeField] private Transform Mesh; //set in editor
 
 
@@ -29,25 +29,33 @@ public class Player : MonoBehaviour {
     public bool isInvincible() { return Invincible; }
     public int PlayerNumber() { return P_Number; }
     public string GetName() { return P_Name; }
-    public Controls getControls() { return P_Controls; }
+    private Controls getControls() { return P_Controls; }
 
-
-    public void Initialize(string Name, uint Number)
+    [ClientRpc]
+    private void RpcInitialize(string Name, int Number)
     {
         P_Number = Number;
         P_Name = Name;
         gameObject.name = "Player" + P_Number;
 
-        //renaming all Objects with the Playernumber at the end
-        for (int i = 0; i < transform.childCount; i++) transform.GetChild(i).name += P_Number;
+        GameManager = transform.GetComponentInParent<Game_Manager>();
 
+        //rename all Objects with the Playernumber at the end
+        for (int i = 0; i < transform.childCount; i++) transform.GetChild(i).name += P_Number;
         //Initialize Children
         transform.GetComponentInChildren<PlayerCamControl>().Initialize(this);
     }
 
+    [Command]
+    public void CmdInitialize(string Name, uint Number)
+    {
+        int Num = checked((int)Number); //checked returns overflow error if uint > int.maxvalue
+        RpcInitialize(Name, Num);
+    }
+
     private void FixedUpdate()
     {
-        if (isAlive() & Game_Manager.CanUpdate()) RecoverStamina(1);
+        if (isAlive() & GameManager.CanUpdate()) RecoverStamina(1);
         //else transform.Find("Info" + P_Number).GetComponent<PlayerFeed>().DeathMessage(Time.time - TimeofDeath, transform.parent.name);
     }
 
@@ -70,23 +78,21 @@ public class Player : MonoBehaviour {
     //turns PlayerModel
     public void OrientateMesh(Vector3 Forward, Vector3 Up)
     {
-        if (Vector3.Magnitude(Forward) >= 0.1f) Mesh.rotation = Quaternion.LookRotation(Forward, Up);
+        if (Vector3.Magnitude(Forward) >= 0.05f) Mesh.rotation = Quaternion.LookRotation(Forward, Up);
     }
     //activates/deactivates Playermodel
     public void ShowMesh(bool MeshActive) { Mesh.gameObject.SetActive(MeshActive); }
 
-
-
     //randomly choose a new Spawnlocation and check if Spawning is safe
     public bool FindNewSpawn()
     {
+        Level_Manager LevelManager = GameManager.getLevelManager();
         RaycastHit Hit;
         Vector3 newPos = Vector3.zero;
-        Quaternion newRot = Quaternion.Euler(0, 0, 0);
 
         //draw Debug Box
-        Vector3 V0 = Game_Manager.Levels().GetSpawnArea()[0];
-        Vector3 V1 = V0 + Game_Manager.Levels().GetSpawnArea()[1];
+        Vector3 V0 = LevelManager.GetSpawnArea()[0];
+        Vector3 V1 = V0 + LevelManager.GetSpawnArea()[1];
         Debug.DrawLine(new Vector3(V0.x, V1.y, V1.z), V1, Color.green, 1000);//Possible SpawnArea
         Debug.DrawLine(new Vector3(V1.x, V1.y, V0.z), V1, Color.green, 1000);
         Debug.DrawLine(new Vector3(V0.x, V0.y, V1.z), V0, Color.green, 1000);
@@ -99,20 +105,18 @@ public class Player : MonoBehaviour {
         do
         {
             //randomly generating Position
-            newPos = Game_Manager.Levels().GetSpawnArea()[0];
-            newPos.x += UnityEngine.Random.value * (Game_Manager.Levels().GetSpawnArea()[1].x);
-            newPos.y += UnityEngine.Random.value * (Game_Manager.Levels().GetSpawnArea()[1].y);
-            newPos.z += UnityEngine.Random.value * (Game_Manager.Levels().GetSpawnArea()[1].z);
-            //randomly generating Rotation
-            newRot = Quaternion.LookRotation(new Vector3(2 * UnityEngine.Random.value - 1, 0, 2 * UnityEngine.Random.value - 1), Vector3.up);
+            newPos = LevelManager.GetSpawnArea()[0];
+            newPos.x += UnityEngine.Random.value * (LevelManager.GetSpawnArea()[1].x);
+            newPos.y += UnityEngine.Random.value * (LevelManager.GetSpawnArea()[1].y);
+            newPos.z += UnityEngine.Random.value * (LevelManager.GetSpawnArea()[1].z);            
             TryAmount++;
+
         } while ((!Physics.Raycast(newPos, Vector3.down, out Hit) || Hit.transform.tag != "CP") && TryAmount <= MaxTries);
         if (TryAmount <= MaxTries)
         {
             Debug.Log("Found new Spawn after " + TryAmount + " tries.");
             Debug.DrawLine(Vector3.zero, newPos, Color.red, 1000); // newfound SpawnPos
             SpawnPos = newPos;
-            SpawnRot = newRot;
             return true;
         }
         else
@@ -122,10 +126,11 @@ public class Player : MonoBehaviour {
         }
     }
 
-
-
     //switch-case for P_Status
-    public void ChangeStatus(int newStatus)
+    [Command]
+    public void CmdChangeStatus(int newStatus) { RpcChangeStatus(newStatus); }
+    [ClientRpc]
+    private void RpcChangeStatus(int newStatus)
         ///Status:
         /// -2: dead, invisible, no collision, invincible, no gravity        (respawning)
         /// -1: dead, visible, no collision, invincible, gravity             (not respawning)
@@ -171,31 +176,37 @@ public class Player : MonoBehaviour {
         }
     }
 
-
-
-    public void Kill()
+    [Command]
+    public void CmdKill() { RpcKill(); }
+    [ClientRpc]
+    private void RpcKill()
     {
         Debug.Log("Player died!");
         Deathcount++;
-        ChangeStatus(-1);
+        CmdChangeStatus(-1);
         Mesh.rotation = Quaternion.LookRotation(Vector3.up);
     }
 
     //usually called (from outside) this delays the Respawn
-    public void RequestRespawn()
+    //runs only on 1 Client!!!!!
+    [Client]
+    private void RequestRespawn()
     {
-        ChangeStatus(-2);
-        Invoke("Respawn", RespawnTime);
+        CmdChangeStatus(-2);
+        Invoke("CmdRespawn", RespawnTime);
     }
 
     //internally called and  exceptionally for instant (re)spawning
-    public void Respawn()
+    [Command]
+    private void CmdRespawn() { RpcRespawn(); }
+    [ClientRpc]
+    private void RpcRespawn()
     {
         if (FindNewSpawn())
         {
             transform.localPosition = SpawnPos;
-            Mesh.rotation = SpawnRot;
-            ChangeStatus(2);
+            Mesh.rotation = Quaternion.LookRotation(new Vector3(2 * UnityEngine.Random.value - 1, 0, 2 * UnityEngine.Random.value - 1), Vector3.up); ;
+            CmdChangeStatus(2);
             SP = MaxSP;
         }
     }  
